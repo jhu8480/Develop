ead93567c22f9cccaf3f6c88abd180e0797f6dd9
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-25 14:15:54 -0400

    added typeDefs, resolvers and refactored server.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+  type User {
+    _id: ID 
+    username: String 
+    email: String 
+    password: String 
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String 
+    authors: [String]
+    description: String 
+    title: String 
+    image: String 
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutations {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User 
+    removeBook(bookId: String): User
+  }
 `;
 
 module.exports = typeDefs;
