ecd432a55ce282d8e0c4a897ca37ac331406f62c
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-25 15:02:46 -0400

    added queries.js and mutations.js in client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,18 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
+export const GET_ME = gql`
   query me {
     me {
       _id
       username
       email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
+      savedBooks {
+        bookId
+        authors
+        description
+        title 
+        image 
+        link
       }
     }
   }
